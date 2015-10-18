c11712c8bfb6f7e515438b2b367b4c1835acb2b6
Chris Gervang <chrisg@tachyus.com>
2015-10-18 10:48:58 -0700

    AWS server test

--- server/collector.py
+++ server/collector.py
@@ -1,7 +1,8 @@
 __author__ = 'chrisgervang'
 
 import sqlite3
-from flask import Flask, request, session
+from flask import Flask, request, session, g, render_template
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/dubhacks_collector.db'
@@ -17,6 +18,30 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def show_entries():
+    # cur = g.db.execute('select title, text from entries order by id desc')
+    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('collector.html')
+
+# @app.route('')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
