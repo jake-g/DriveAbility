5f0027a36c472976c426d0dc9bd044a76cabaa26
Chris Gervang <chrisg@tachyus.com>
2015-10-18 20:03:39 -0700

    WOO WEBPACK ... start

--- collector.py
+++ collector.py
@@ -1,11 +1,14 @@
 __author__ = 'chrisgervang'
 
 import sqlite3
-from flask import Flask, request, g, render_template, session, jsonify
+from flask import Flask, request, g, render_template, session, jsonify, Response
 from contextlib import closing
 import numpy as np
 import datetime
 import json
+import os
+from flask_webpack import Webpack
+
 
 # configuration
 DATABASE = '/tmp/dubhacks_collector.db'
@@ -18,7 +21,9 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 # app.secret_key = 'some key for session'
+webpack = Webpack()
 
+webpack.init_app(app)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -28,6 +33,22 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+def root_dir():  # pragma: no cover
+    print os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "app", "src"))
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "app", "src"))
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
 
 @app.before_request
 def before_request():
@@ -43,8 +64,25 @@ def teardown_request(exception):
 def show_entries():
     # cur = g.db.execute('select title, text from entries order by id desc')
     # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('collector.html')
+    return Response(open('../app/src/index.html').read(), mimetype="text/html")
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+    }
+    complete_path = os.path.join(root_dir(), path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
+
+@app.route('/drive')
+def drive():
+    return render_template('collector.html')
 @app.route('/carride', methods=['POST'])
 def post_carride():
     obj = {"ride": eval(request.json["ride"])}
